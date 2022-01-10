9d61d93216b29b702ed7f1a8b1e7b77ba6e51dde
PaperScorpio <82784617+PaperScorpio@users.noreply.github.com>
2022-01-09 17:43:13 -0800

    Use ClientEventTicks instead of MinecraftClientMixin

--- src/main/java/com/paperscp/sprintometer/mixins/MinecraftClientMixin.java
+++ src/main/java/com/paperscp/sprintometer/mixins/MinecraftClientMixin.java
@@ -1,23 +1,11 @@
 package com.paperscp.sprintometer.mixins;
 
-import com.paperscp.sprintometer.SprintOMeter;
-import com.paperscp.sprintometer.client.ActionStamina;
-import com.paperscp.sprintometer.client.ui.StaminaHudManager;
 import net.minecraft.client.MinecraftClient;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftClient.class)
 public class MinecraftClientMixin {
-    @Inject(at = @At("TAIL"), method = "tick")
-    public void tick(CallbackInfo ci) {
-        SprintOMeter.tick();
-
-    }
-
     @Mixin(MinecraftClient.class)
     public interface MinecraftClientInterfaceMixin {
         @Accessor("currentFps")
