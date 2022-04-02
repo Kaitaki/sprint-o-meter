f5cf579cd8da16da9d9688d82145c4bc49901889
PaperScorpio <82784617+PaperScorpio@users.noreply.github.com>
2022-04-01 20:23:08 -0700

    Minor Cleanup

--- src/main/java/com/paperscp/sprintometer/mixins/MinecraftClientInterfaceMixin.java
+++ src/main/java/com/paperscp/sprintometer/mixins/MinecraftClientInterfaceMixin.java
@@ -5,12 +5,9 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(MinecraftClient.class)
-public class MinecraftClientMixin {
-    @Mixin(MinecraftClient.class)
-    public interface MinecraftClientInterfaceMixin {
-        @Accessor("currentFps")
-        int getCurrentFPS();
-    }
+public interface MinecraftClientInterfaceMixin {
+    @Accessor("currentFps")
+    int getCurrentFPS();
 }
 
 
