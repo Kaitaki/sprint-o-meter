Kaitaki/sprint-o-meter

commit ffb363e312bb8141acd2ddd9143199cba5a46825

Fix Issue #9

src/main/java/com/paperscp/sprintometer/client/StaminaManager.java
@@ -94,7 +94,7 @@ public class StaminaManager {
         if (staminaDeductionDelay == 0) {
             deductStamina();
             staminaDeductionDelay = getConfig(STAMINADEDUCTIONDELAY);
-            if (stamina <= quarterStamina && (isSprinting || isJumping) && sprintConfig.lowStaminaWarn) {
+            if (stamina <= quarterStamina && (isSprinting || isJumping) && sprintConfig.enableLowStaminaWarn) {
                 player.playSound(SoundEvents.ENTITY_EXPERIENCE_ORB_PICKUP, 0.1f, 2);
             }
         } else { staminaDeductionDelay--; }
@@ -120,10 +120,10 @@ public class StaminaManager {
 
     private boolean isJumping(boolean isJumpingAccurate) {
         if (getConfig(VERTICALSWIMDEDUCT) == 0) {
-            if (isInWater) {return false;}
+            if (isInWater) { return false; }
         }
 
-        if (player.verticalCollision) {jumped = false;}
+        if (player.verticalCollision) { jumped = false;}
 
         return isJumpingAccurate && player.fallDistance == 0.0;
     }
@@ -176,16 +176,28 @@ public class StaminaManager {
         if (hasStaminaGainEffect()) { return; }
         if (!isSprinting && !isJumping) { return; }
 
-        if (isSprinting) { stamina = stamina - getConfig(SPRINTDEDUCTIONAMOUNT); cooldownDelay = getConfig(COOLDOWNDELAY); } // Sprint Deduct
+        if (isSprinting) {
+            int sprintDeductAmt = getConfig(SPRINTDEDUCTIONAMOUNT);
 
-        if (isJumping && !jumped) { stamina = stamina - getConfig(JUMPDEDUCTIONAMOUNT); cooldownDelay = getConfig(COOLDOWNDELAY); jumped = true;} // Jump Deduct
+            stamina = stamina - sprintDeductAmt;
+            if (sprintDeductAmt == 0) { return; }
+            cooldownDelay = getConfig(COOLDOWNDELAY);
+        } // Sprint Deduct
+
+        if (isJumping && !jumped) {
+            int jumpDeductAmt = getConfig(JUMPDEDUCTIONAMOUNT);
+
+            stamina = stamina - jumpDeductAmt;
+            jumped = true;
+            if (jumpDeductAmt == 0) { return; }
+            cooldownDelay = getConfig(COOLDOWNDELAY);
+        } // Jump Deduct
 
     }
 
     private void restoreStamina() {
         if (stamina == maxStamina) { return; }
 
-        if (isSprinting || isJumping) { return; }
         if (isInWater) { restoreStaminaAlternate(); return; }
         if (cooldownDelay != 0) { cooldownDelay--; return; } // Cooldown Delay
 

src/main/java/com/paperscp/sprintometer/config/ConfiguratorOptions.java
@@ -10,5 +10,6 @@ public enum ConfiguratorOptions {
     ISENABLED,
     MAXSTAMINA,
     VERTICALSWIMDEDUCT,
-    DEDUCTWITHPOTIONEFFECT
+    DEDUCTWITHPOTIONEFFECT,
+    JUMPDEBUFF
 }

src/main/java/com/paperscp/sprintometer/config/SprintConfigurator.java
@@ -18,7 +18,7 @@ public class SprintConfigurator {
         // Server Packet Array
         private static int[] configArray;
 
-        public static final short configAmount = 10;
+        public static final short configAmount = 11;
 
         public static int getConfig(ConfiguratorOptions configOption) {
             if (configArray == null) {return 0;} // Failsafe, 0 until config is loaded/sent
@@ -44,6 +44,8 @@ public class SprintConfigurator {
                     return configArray[8];
                 case DEDUCTWITHPOTIONEFFECT:
                     return configArray[9];
+                case JUMPDEBUFF:
+                    return configArray[10];
 
                 default:
                     SprintOMeterServer.logger.fatal("Something happened while trying to get config values!");

src/main/java/com/paperscp/sprintometer/config/SprintOConfig.java
@@ -22,9 +22,11 @@ public class SprintOConfig implements ConfigData {
     @ConfigEntry.Gui.Tooltip(count = 3)
     public boolean enableSprintOMeter = true;
 
+    @ConfigEntry.Gui.Tooltip(count = 3)
+    public boolean enableJumpDebuff = true;
 
     @ConfigEntry.Gui.Tooltip(count = 3)
-    public boolean lowStaminaWarn = true; // Not sent to client (Client Sided Config)
+    public boolean enableLowStaminaWarn = true; // Not sent to client (Client Sided Config)
 
 //    @ConfigEntry.Gui.EnumHandler(option = ConfigEntry.Gui.EnumHandler.EnumDisplayOption.BUTTON)
 //    @ConfigEntry.Gui.Tooltip(count = 3)
@@ -109,7 +111,7 @@ public class SprintOConfig implements ConfigData {
 
     @ConfigEntry.Category("misc")
     @ConfigEntry.Gui.Tooltip(count = 3)
-    public boolean verticalSwimDeduct = false;
+    public boolean enableVerticalSwimDeduct = false;
 
     @ConfigEntry.Category("misc")
     @ConfigEntry.Gui.Tooltip(count = 3)
@@ -147,8 +149,9 @@ public class SprintOConfig implements ConfigData {
         cp.addConfig(sprintConfig.staminaRestorationAmount); // 5
         cp.addConfig(sprintConfig.enableSprintOMeter); // 6
         cp.addConfig(sprintConfig.maximumStamina); // 7
-        cp.addConfig(sprintConfig.verticalSwimDeduct); // 8
+        cp.addConfig(sprintConfig.enableVerticalSwimDeduct); // 8
         cp.addConfig(sprintConfig.deductWithPotionEffect); // 9
+        cp.addConfig(sprintConfig.enableJumpDebuff); // 10
 
         cp.sendPacket();
 

src/main/java/com/paperscp/sprintometer/mixins/KeyboardInputMixin.java
@@ -1,6 +1,8 @@
 package com.paperscp.sprintometer.mixins;
 
 import com.paperscp.sprintometer.client.StaminaManager;
+import com.paperscp.sprintometer.config.ConfiguratorOptions;
+import com.paperscp.sprintometer.config.SprintConfigurator;
 import net.minecraft.client.input.Input;
 import net.minecraft.client.input.KeyboardInput;
 import net.minecraft.client.option.GameOptions;
@@ -27,6 +29,7 @@ public class KeyboardInputMixin extends Input {
     public void tick(boolean slowDown, CallbackInfo ci) {
         StaminaManager.isJumpKeyPressed = this.settings.keyJump.isPressed();
 
+        if (SprintConfigurator.getConfig(ConfiguratorOptions.JUMPDEBUFF) == 0) { return; }
         this.jumping = !StaminaManager.isOutOfStamina() && this.settings.keyJump.isPressed();
     }
 

commit 3d965377cdd54b1debf69bb0487a2e85fc2fe26e

Fix Bug Caused By Commit ffb363e

src/main/java/com/paperscp/sprintometer/client/StaminaManager.java
@@ -180,8 +180,9 @@ public class StaminaManager {
             int sprintDeductAmt = getConfig(SPRINTDEDUCTIONAMOUNT);
 
             stamina = stamina - sprintDeductAmt;
-            if (sprintDeductAmt == 0) { return; }
-            cooldownDelay = getConfig(COOLDOWNDELAY);
+            if (sprintDeductAmt != 0) {
+                cooldownDelay = getConfig(COOLDOWNDELAY);
+            }
         } // Sprint Deduct
 
         if (isJumping && !jumped) {
@@ -189,8 +190,9 @@ public class StaminaManager {
 
             stamina = stamina - jumpDeductAmt;
             jumped = true;
-            if (jumpDeductAmt == 0) { return; }
-            cooldownDelay = getConfig(COOLDOWNDELAY);
+            if (jumpDeductAmt != 0) {
+                cooldownDelay = getConfig(COOLDOWNDELAY);
+            }
         } // Jump Deduct
 
     }